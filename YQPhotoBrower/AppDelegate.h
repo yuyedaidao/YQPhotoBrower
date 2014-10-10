703e4235e50efebbd6c67c46289db2ae89d7af9d
wish <wyqpadding@163.com>
2014-10-10 20:24:06 +0800

    Initial Commit

---
+++ YQPhotoBrower/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQPhotoBrower
+//
+//  Created by Leaf on 14-10-10.
+//  Copyright (c) 2014年 Leaf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQPhotoBrower/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQPhotoBrower
+//
+//  Created by Leaf on 14-10-10.
+//  Copyright (c) 2014年 Leaf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
